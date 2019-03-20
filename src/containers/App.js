c2dac5d02de79ec3bce21b25447ef8b689e6e070
Umer Khan <umer.kdev@gmail.com>
2019-03-20 23:51:07 +0200

    testing store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import '../App.css';
+import {Provider} from 'react-redux'
 import Post from '../components/Post'
 import Postform from '../components/Postform'
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware)
 
 class App extends Component {
   render() {
     return (
+      <Provider store = {store}>
       <div className="App">
        <h1>Posts</h1>
        <Postform />
        <Post />
       </div>
+      </Provider>
     );
   }
 }
