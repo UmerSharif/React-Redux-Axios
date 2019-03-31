69859566e49edb64a699b81a78dfaa0d89a1f182
umer <umer4167@hotmail.com>
2019-03-31 22:21:43 +0300

    setting up redux dev tools

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import {connect} from 'react-redux'
 import fetchPost from '../actions/postAction'
+import PropTypes from 'prop-types';
 
 class Post extends Component {
 
@@ -25,6 +26,11 @@ class Post extends Component {
     console.log(postdata);
     return <div>{postdata}</div>;
   }
+}
+
+Post.propTypes = {
+  fetchPost: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
 }
 
 const mapStateToProps = state => ({
--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer'
 
@@ -7,6 +7,10 @@ const initialState = {}
 const middleWares = [thunk]
 
 
-const store = createStore(rootReducer, initialState, applyMiddleware(...middleWares))
-
+const store = createStore(rootReducer, initialState, 
+    compose(
+    applyMiddleware(...middleWares),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+)
 export default store
